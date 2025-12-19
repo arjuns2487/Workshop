f97e3da20bb773e2c26b1d04135a7618ef430ffc
Nigachad69 <Nigachad69@users.noreply.github.com>
2025-12-19 14:21:44 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask,render_template,jsonify,request
+import pandas as pd
+
+from sklearn import linear_model
+df=pd.read_csv('./homeprices.csv')
+median=df['bedrooms'].median()
+df.fillna(median,inplace=True)
+fea=df.drop('price',axis=1)
+labels=df.price
+
+model=linear_model.LinearRegression()
+model.fit(fea,labels)
+
+app=Flask(__name__)
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data=request.json
+    area=float(data["area"])
+    age=float(data["age"])
+    bed=float(data["bedrooms"])
+    print("The area is : ",area)
+    print("Age : ",age)
+    print("Bedrooms : ",bed)
+    predicted_value=model.predict([[area,bed,age]])
+    return jsonify({"predicted_value":predicted_value[0]})
+
+if __name__=="__main__":
+    app.run(debug=True)
